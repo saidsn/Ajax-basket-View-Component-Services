1764c289d1b427411d197826d5ab5857056b1cbb
saidsn <saidsn@code.edu.az>
2022-10-18 01:11:25 +0400

    Homework

---
+++ EntityFrameworkProject/Data/AppDbContext.cs
@@ -0,0 +1,95 @@
+﻿using EntityFrameworkProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EntityFrameworkProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<SliderDetail> SliderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        
+        protected  override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>().HasQueryFilter(m => !m.IsDeleted);
+            modelBuilder.Entity<Setting>().HasQueryFilter(m => !m.IsDeleted);
+            modelBuilder.Entity<Blog>().HasData(
+            new Blog
+            {
+                Id = 1,
+                IsDeleted = false,
+                Title = "Blog-1",
+                Desc = "Desc-1",
+                Image = "blog-feature-img-1.jpg",
+                Date = DateTime.Now
+            },
+
+
+            new Blog
+            {
+                Id = 2,
+                IsDeleted = false,
+                Title = "Blog-2",
+                Desc = "Desc-2",
+                Image = "blog-feature-img-3.jpg",
+                Date = DateTime.Now
+            },
+
+            new Blog
+            {
+                Id = 3,
+                IsDeleted = false,
+                Title = "Blog-3",
+                Desc = "Desc-3",
+                Image = "blog-feature-img-4.jpg",
+                Date = DateTime.Now
+            }
+
+            );
+
+            modelBuilder.Entity<Setting>().HasData(
+            new Setting
+            {
+                Id = 1,
+                IsDeleted = false,
+                Key = "Header Logo",
+                Value = "logo.png"
+               
+            },
+            new Setting
+            {
+                Id = 2,
+                IsDeleted = false,
+                Key = "Phone",
+                Value = "64648393930039"
+
+            },
+            new Setting
+            {
+                Id = 3,
+                IsDeleted = false,
+                Key = "ProductTake",
+                Value = "4"
+
+            },
+            new Setting
+            {
+                Id = 4,
+                IsDeleted = false,
+                Key = "CompanyBoss",
+                Value = "Bakhtiyar Shamilzada"
+            }
+            );
+
+        }
+    }
+}
---
+++ EntityFrameworkProject/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using EntityFrameworkProject.Data;
+using EntityFrameworkProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public HeaderViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            return await Task.FromResult(View(await GetAllSettings()));
+        }
+        private async Task<IEnumerable<Setting>> GetAllSettings() => await _context.Settings.ToListAsync();
+    }
+}
